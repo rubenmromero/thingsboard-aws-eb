faf4baf33700b82f19ec2096914344e67d86928d
Ruben Martin <rubenmromero@gmail.com>
2020-04-11 19:47:47 +0200

    Initial project import

---
+++ README.md
@@ -0,0 +1,160 @@
+# ThingsBoard-AWS-EB
+
+[ThingsBoard](https://thingsboard.io/) dockerized application deployment to [AWS Elastic Beanstalk](https://aws.amazon.com/elasticbeanstalk/).
+
+![ThingsBoard](thingsboard.gif)
+![AWS Elastic Beanstalk + Docker](aws_eb-docker.jpeg)
+
+# ThingsBoard
+
+[ThingsBoard](https://thingsboard.io/docs/getting-started-guides/what-is-thingsboard/) is an open-source IoT platform that enables rapid development, management and scaling of IoT projects. Our goal is to provide the out-of-the-box IoT cloud or on-premises solution that will enable server-side infrastructure for your IoT applications.
+
+## Local Execution
+
+To run locally the dockerized ThingsBoard application, follow the instructions described in these links:
+
+* [Installing ThingsBoard CE using Docker (Linux or Mac OS)](https://thingsboard.io/docs/user-guide/install/docker/)
+* [thingsboard/tb-postgres - Docker Hub](https://hub.docker.com/r/thingsboard/tb-postgres/)
+
+# Deployment
+
+## How does it work?
+
+The `deploy.py` CLI deploys the ThingsBoard application to AWS Elastic Beanstalk performing the following tasks:
+
+1. Create a boto3 session to store the AWS credentials and the region to use for the deployment. If there is any configuration defined through [environment variables](https://docs.aws.amazon.com/cli/latest/userguide/cli-configure-envvars.html) or stored in AWS CLI `~/.aws` folder, the AWS credentials and the region are directly retrieved from there (it is possible to specify the profile to use as optional argument). Otherwise, the CLI asks for these parameters interactively.
+2. Check if the Elastic Beanstalk application is already created, and if not, create it.
+3. Create the ZIP package (source bundle) for the new application version.
+4. Upload the application version package to a source bundle S3 bucket.
+5. Create the new application version for the Elastic Beanstalk application.
+6. Create the environment if it does not exist or has been previously terminated, or update it if it is already created. The CLI check if there is an operation in progress in the environment and wait for it to be completed before updating or recreating it.
+
+## Dependencies
+
+* `Python 3.3+`
+* `boto3`
+
+## Prerequisites
+
+* The `aws-elasticbeanstalk-ec2-role` instance profile (AWS IAM EC2 Role) created in the AWS account:
+
+    [Managing Elastic Beanstalk instance profiles](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/iam-instanceprofile.html)
+
+* The `aws-elasticbeanstalk-service-role` service role (AWS IAM Elastic Beanstalk Role) created in the AWS account:
+
+    [Managing Elastic Beanstalk service roles](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/iam-servicerole.html)
+
+* An IAM User or an AWS IAM Role attached to the EC2 instance (only for executions from EC2 instances) with the following IAM Managed Policy attached:
+
+      AWSElasticBeanstalkFullAccess
+
+* Pip tool for Python packages management. Installation:
+
+      # From Linux
+      $ curl -O https://bootstrap.pypa.io/get-pip.py
+      $ sudo python3 get-pip.py
+
+      # From macOS
+      $ brew install python
+
+* AWS SDK for Python. Installation:
+
+      # From Linux
+      $ sudo pip3 install boto3
+
+      # From macOS
+      $ pip3 install boto3
+
+## Execution Method
+
+Here you have the message that you will get if you request help to the `deploy.py` CLI:
+
+    $ ./deploy.py --help
+    usage: deploy.py [-h] -a APPLICATION_NAME -e ENVIRONMENT_NAME [-p PROFILE]
+
+    Custom CLI to deploy an application to AWS Elastic Beanstalk
+
+    optional arguments:
+      -h, --help            show this help message and exit
+
+    Options:
+      -a APPLICATION_NAME, --application-name APPLICATION_NAME
+                            Name of the Elastic Beanstalk application
+      -e ENVIRONMENT_NAME, --environment-name ENVIRONMENT_NAME
+                            Name of the environment for the Elastic Beanstalk
+                            application
+      -p PROFILE, --profile PROFILE
+                            Use a specific profile from AWS CLI stored
+                            configurations
+
+## Provided Configuration Profiles
+
+This project provides the following configuration profiles:
+
+* `env.yaml.staging` => Configuration profile intended for staging environments deployed for development, test, QA or demo purposes (lower cost and performance)
+* `env.yaml.production` => Configuration profile intended for environments with productive loads
+
+## Execution Examples
+
+    # Deployment of a staging environment
+    $ ln -sf env.yaml.staging env.yaml
+    $ ./deploy.py --application-name thingsboard --environment-name demo [--profile <aws_cli_profile>]
+
+    # Deployment of a productive environment
+    $ ln -sf env.yaml.production env.yaml
+    $ ./deploy.py --application-name thingsboard --environment-name live [--profile <aws_cli_profile>]
+
+## Environment Customization
+
+If you need to modify any settings provided in this project or add new ones, feel free to fork or import this repository and apply the changes you need to fulfil your use case.
+
+Here are some common customizations that you might want to add to an environment:
+
+* To deploy a `SingleInstance` type environment in a existing custom VPC, add the following configuration block to the `env.yaml.staging` file:
+
+        aws:ec2:vpc:
+          VPCId: <vpc_id>
+          Subnets: <subnet_1a_id>,<subnet_1b_id>,...      # Subnet IDs for EC2 instance
+          DBSubnets: <subnet_2a_id>,<subnet_2b_id>,...    # Subnet IDs for RDS instance
+
+* To deploy a `LoadBalanced` type environment in a existing custom VPC, add the following settings to the existing `aws:ec2:vpc` namespace in `env.yaml.production` file:
+
+        aws:ec2:vpc:
+          ELBScheme: public                               # This setting is already defined
+          VPCId: <vpc_id>
+          ELBSubnets: <subnet_3a_id>,<subnet_3b_id>,...   # Subnet IDs for ELB
+          Subnets: <subnet_1a_id>,<subnet_1b_id>,...      # Subnet IDs for Auto Scaling group
+          DBSubnets: <subnet_2a_id>,<subnet_2b_id>,...    # Subnet IDs for RDS instance
+          AssociatePublicIpAddress: true                  # Review this option in https://amzn.to/2Vhgt5B
+
+* To configure HTTPS in a `LoadBalanced` type environment for a custom domain using an ACM issued certificate, add the following configuration block to the `env.yaml.production` file:
+
+        aws:elb:listener:443:
+          ListenerProtocol: HTTPS
+          InstancePort: '80'
+          InstanceProtocol: HTTP
+          ListenerEnabled: true
+          SSLCertificateId: <acm_certificate_arn>
+
+    Then uncomment the `https-redirect-docker-sc` container command block in `.ebextensions/server-updates.config` file:
+
+        https-redirect-docker-sc:
+          command: cp .ebextensions/nginx/elasticbeanstalk-nginx-docker-proxy.conf /etc/nginx/sites-available/
+          test: '[ "$(/opt/elasticbeanstalk/bin/get-config environment -k CONFIG_PROFILE)" == "production" ]'
+
+* To configure a SSH key pair to securely log into the EC2 instance/s belonging to an environment, add the following setting to the existing `aws:autoscaling:launchconfiguration` namespace in any of the `env.yaml.<config_profile>` files:
+
+        aws:autoscaling:launchconfiguration:
+          . . .
+          . . .
+          EC2KeyName: <key_pair_name>
+
+## Related Links
+
+* [Installing ThingsBoard CE on CentOS/RHEL](https://thingsboard.io/docs/user-guide/install/rhel/)
+* [Single Container Docker Configuration](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/single-container-docker-configuration.html)
+* [Environment Manifest (env.yaml)](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/environment-cfg-manifest.html)
+* [General options for all environments](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/command-options-general.html)
+* [Configuring HTTP to HTTPS redirection](https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/configuring-https-httpredirect.html)
+* [AWS SDK for Python (Boto3)](https://aws.amazon.com/sdk-for-python/)
+* [ElasticBeanstalk - Boto 3 Docs](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/elasticbeanstalk.html)
---
+++ deploy.py
@@ -0,0 +1,234 @@
+#!/usr/bin/env python3
+
+#
+# Modules Import
+#
+import argparse
+import boto3
+import glob
+import json
+import os
+import shlex
+import shutil
+import subprocess
+import sys
+import time
+from zipfile import ZipFile
+
+#
+# Variables Definition
+#
+app_ver_pkg_name = 'thingsboard-' + time.strftime('%Y%m%d-%H%M%S')
+elasticbeanstalk_paths = ['Dockerrun.aws.json', 'env.yaml', '.ebextensions']
+source_bundle_s3_bucket_prefix = 'elasticbeanstalk'
+
+#
+# Function to parse the input arguments and build the help message
+#
+def arguments_parser():
+    parser = argparse.ArgumentParser(
+        description='Custom CLI to deploy an application to AWS Elastic Beanstalk',
+        add_help=True
+    )
+
+    options = parser.add_argument_group('Options')
+    options.add_argument(
+        '-a', '--application-name',
+        type=str,
+        action='store',
+        dest='application_name',
+        required=True,
+        help='Name of the Elastic Beanstalk application'
+    )
+    options.add_argument(
+        '-e', '--environment-name',
+        type=str,
+        action='store',
+        dest='environment_name',
+        required=True,
+        help='Name of the environment for the Elastic Beanstalk application'
+    )
+    options.add_argument(
+        '-p', '--profile',
+        type=str,
+        action='store',
+        dest='profile',
+        default='default',
+        help='Use a specific profile from AWS CLI stored configurations'
+    )
+
+    args = parser.parse_args()
+    return args
+
+#
+# Function to print the result of system commands executions
+#
+def print_result(output, error):
+    if output != '':
+        print(output)
+    if error != '':
+        print(error)
+
+#
+# Function to print the boto3 responses in JSON format
+#
+def print_response(response):
+    print(json.dumps(
+        response,
+        default=str,
+        sort_keys=True,
+        indent=4,
+        separators=(',', ': ')
+    ))
+
+#
+# Function to create a session of boto3 to interact with the AWS account
+#
+def create_session():
+    profile = arguments.profile
+    if (profile != 'default') and (not profile in boto3.session.Session().available_profiles):
+        print("\nThe '" + profile + "' profile does not exist!\n")
+    elif (profile == 'default') and (boto3.session.Session().get_credentials() is None):
+        print("\nThere is no AWS CLI configuration defined!\n")
+    elif profile != 'default':
+        return boto3.session.Session(profile_name=profile)
+    else:
+        return boto3.session.Session()
+
+    #print("Please provide AWS configuration, e.g. via the AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY and AWS_DEFAULT_REGION environment variables\n")
+    #exit(-1)
+    access_key_id = input("Enter the AWS_ACCESS_KEY_ID of the AWS account in which to deploy the application: ")
+    secret_access_key = input("Enter the AWS_SECRET_ACCESS_KEY of the AWS account in which to deploy the application: ")
+    region = input("Enter the region in which to deploy the application: ")
+
+    return boto3.Session(
+        aws_access_key_id=access_key_id,
+        aws_secret_access_key=secret_access_key,
+        region_name=region,
+    )
+
+#
+# Function to create the ZIP package for the new application version
+#
+def package_application():
+    with ZipFile(app_ver_pkg_name + '.zip', 'w') as zip:
+        for path in elasticbeanstalk_paths:
+            if os.path.isdir(path):
+                 for root, dirs, files in os.walk(path):
+                     for file in files:
+                         zip.write(os.path.join(root, file))
+            else:
+                zip.write(path)
+
+    print("All application files zipped successfully to '" + app_ver_pkg_name + ".zip' package\n")
+    return app_ver_pkg_name
+
+#
+# Function to upload an application version package to a source bundle S3 bucket
+#
+def upload_application_version():
+    sts = session.client('sts')
+    s3_bucket = source_bundle_s3_bucket_prefix + '-' + session.region_name + '-' + sts.get_caller_identity()['Account']
+    s3 = session.resource('s3')
+    if not s3.Bucket(s3_bucket) in s3.buckets.all():
+        s3.create_bucket(
+            ACL='private',
+            Bucket=s3_bucket,
+            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+        )
+    response = s3.Bucket(s3_bucket).put_object(
+        Body=open(application_version_package_name + '.zip', 'rb'),
+        Key=application_version_package_name + '.zip'
+    )
+    print(response)
+    return s3_bucket
+
+#
+# Main
+#
+
+# Change directory to the project root folder
+os.chdir(os.path.dirname(__file__))
+
+# Parse input arguments
+arguments = arguments_parser()
+
+# Check if an 'env.yaml' file exists in the project root folder
+if not os.path.isfile('env.yaml'):
+    print("\nThere is no 'env.yaml' environment configuration file created in the project root folder")
+    print("\nPlease create this file and run again the CLI executing the following commands:")
+    print("\n\t$ ln -sf env.yaml.<config_profile> env.yaml")
+    print("\t$ " + ' '.join(sys.argv[0:]) + "\n")
+    exit(-1)
+
+session = create_session()
+elasticbeanstalk = session.client('elasticbeanstalk')
+
+# Check if the Elastic Beanstalk application is already created
+application_name = arguments.application_name
+if not elasticbeanstalk.describe_applications(ApplicationNames=[application_name])['Applications']:
+    print("\nCreate '" + application_name + "' Elastic Beanstalk application:\n")
+    response = elasticbeanstalk.create_application(
+        ApplicationName=application_name,
+        Description=application_name + ' Application'
+    )
+    print_response(response)
+
+print("\nCreate the ZIP package for the new application version:\n")
+application_version_package_name = package_application()
+
+print("\nUpload the application version package to a source bundle S3 bucket:\n")
+source_bundle_s3_bucket = upload_application_version()
+
+print("\nCreate '" + application_version_package_name + "' application version for '" + application_name + "' Elastic Beanstalk application:\n")
+response = elasticbeanstalk.create_application_version(
+    ApplicationName=application_name,
+    VersionLabel=application_version_package_name,
+    SourceBundle={
+        'S3Bucket': source_bundle_s3_bucket,
+        'S3Key': application_version_package_name + '.zip'
+    },
+    AutoCreateApplication=False,
+    Process=True
+)
+print_response(response)
+
+# Wait a few seconds while the created application version is processed
+time.sleep(10)
+
+environment_name = application_name + '-' + arguments.environment_name
+
+# Check if there is an operation in progress in the environment and
+# wait for it to be completed before updating or recreating it
+environment_configuration = elasticbeanstalk.describe_environments(
+    ApplicationName=application_name,
+    EnvironmentNames=[environment_name]
+)['Environments']
+wait_message_printed = False
+while (environment_configuration) and (environment_configuration[0]['Status'] != 'Ready') and (environment_configuration[0]['Status'] != 'Terminated'):
+    if not wait_message_printed:
+        print("\nThere is an operation in progress in '" + environment_name + "' environment. Waiting for it to be completed...\n")
+        wait_message_printed = True
+    time.sleep(30)
+    environment_configuration = elasticbeanstalk.describe_environments(
+        ApplicationName=application_name,
+        EnvironmentNames=[environment_name]
+    )['Environments']
+
+if (not environment_configuration) or (environment_configuration[0]['Status'] == 'Terminated'):
+    print("\nCreate '" + environment_name + "' environment for '" + application_name + "' Elastic Beanstalk application:\n")
+    response = elasticbeanstalk.create_environment(
+        ApplicationName=application_name,
+        EnvironmentName=environment_name,
+        Description=environment_name + ' Environment',
+        CNAMEPrefix=environment_name,
+        VersionLabel=application_version_package_name
+    )
+else:
+    print("\nUpdate '" + environment_name + "' environment for '" + application_name + "' Elastic Beanstalk application:\n")
+    response = elasticbeanstalk.update_environment(
+        ApplicationName=application_name,
+        EnvironmentName=environment_name,
+        VersionLabel=application_version_package_name
+    )
+print_response(response)
